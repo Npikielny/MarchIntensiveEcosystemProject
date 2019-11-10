bcd1309d014d883c51a2226e92ea3b43de5572c7
Noah Pikielny <npikielny@gmail.com>
2019-11-10 12:00:11 -0500

    Initial Commit

---
+++ EcosystemBackEnd/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  EcosystemBackEnd
+//
+//  Created by Noah Pikielny on 11/10/19.
+//  Copyright © 2019 Noah Pikielny. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
