6bff51fbd89d517d8195b453294b73593ce9b4ed
Noah Pikielny <npikielny@gmail.com>
2019-11-12 10:07:19 -0500

    Acacia Trees

--- EcosystemBackEnd/MeshRenderer.swift
+++ EcosystemBackEnd/MeshRenderer.swift
@@ -85,100 +85,99 @@ class SceneGenerator {
 //    var water: Mesh
 //    var pineGen: PineGenerator
 //    var noiseMap: GKNoiseMap
-//    init(width: CGFloat, height: CGFloat, widthCount: Int, heightCount: Int) {
-//        var vertices = [SCNVector3]()
-//            noiseMap = {
-//                let source = GKPerlinNoiseSource()
-//                source.persistence = 0.9
-//                let noise = GKNoise(source)
-//                let size = vector2(1.0, 1.0)
-//                let origin = vector2(0.0, 0.0)
-//                let sampleCount = vector2(Int32(widthCount), Int32(heightCount))
-//                return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
-//            }()
-//            for w in 0..<widthCount {
-//                for h in 0..<heightCount {
-//                    let vertex = SCNVector3(x: width*CGFloat(w)/CGFloat(widthCount-1)-width/2, y: 1-CGFloat(noiseMap.interpolatedValue(at: vector_float2(Float(w),Float(h)))), z: height*CGFloat(h)/CGFloat(heightCount-1)-height/2)
-//                    vertices.append(vertex)
-//                }
-//            }
-//
-//            var groundIndices = [UInt16]()
-//            var waterIndices = [UInt16]()
-//
-//            let index: (Int,Int) -> Int = {return $0 * heightCount + $1}
-//
-//
-//            for w in 0..<widthCount-1 {
-//                for h in 0..<heightCount-1 {
-//                    var squareVerticies = [UInt16]()
-//                    squareVerticies.append(UInt16(index(w,h)))
-//                    squareVerticies.append(UInt16(index(w,h+1)))
-//                    squareVerticies.append(UInt16(index(w+1,h)))
-//                    squareVerticies.append(UInt16(index(w+1,h+1)))
-//
-//                    var water: Bool = false
-//
-//                    for i in [index(w,h),index(w,h+1),index(w+1,h),index(w+1,h+1)] {
-//                        if vertices[i].y < 2 {
-//                            water = true
-//                        }
-//                    }
-//
-//                    groundIndices.append(squareVerticies[0])
-//                    groundIndices.append(squareVerticies[1])
-//                    groundIndices.append(squareVerticies[3])
-//
-//                    groundIndices.append(squareVerticies[0])
-//                    groundIndices.append(squareVerticies[3])
-//                    groundIndices.append(squareVerticies[2])
-//
-//                    if water == true {
-//                        waterIndices.append(squareVerticies[0])
-//                        waterIndices.append(squareVerticies[1])
-//                        waterIndices.append(squareVerticies[3])
-//
-//                        waterIndices.append(squareVerticies[0])
-//                        waterIndices.append(squareVerticies[3])
-//                        waterIndices.append(squareVerticies[2])
-//                    }
-//
-//                }
-//            }
-//        ground = Mesh(Verticies: vertices, Indices: groundIndices)
-//        ground.node.name = "Ground"
-//        let color1: NSColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-//        let color2: NSColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
-//        ground.node.geometry?.materials.first?.roughness.contents = 1
-//        ground.node.geometry?.materials.first!.shaderModifiers = [.geometry :
-//            getShader(from: "groundShader")
-//        ]
-//
-//
-//
-//
-//
-//        water = Mesh(Verticies: vertices, Indices: waterIndices)
-//        water.node.name = "Water"
-//
-//        let color: NSColor = #colorLiteral(red: 0, green: 0.9859127402, blue: 1, alpha: 0.5554098887)
-//        water.node.geometry?.materials.first!.diffuse.contents = color
-//        water.node.geometry?.materials.first?.metalness.contents = 1
-//        water.node.geometry?.materials.first?.roughness.contents = 0
-//        water.node.geometry?.materials.first?.multiply.contents = NSColor.white
-//        water.node.geometry?.materials.first?.shininess = 1
-//        water.node.geometry?.materials.first?.specular.contents = 1
-//        water.node.geometry?.materials.first!.shaderModifiers = [.geometry :
-//            getShader(from: "waterShader")
-//        ]
-//
-//
-//
-//
-//        pineGen = PineGenerator(NumberOfPines: 500, NoiseMap: noiseMap, Width: 400, Height: 400, widthCount: 100, heightCount: 100)
-//
-//    }
-//
+////    init(width: CGFloat, height: CGFloat, widthCount: Int, heightCount: Int) {
+////        var vertices = [SCNVector3]()
+////            noiseMap = {
+////                let source = GKPerlinNoiseSource()
+////                source.persistence = 0.9
+////                let noise = GKNoise(source)
+////                let size = vector2(1.0, 1.0)
+////                let origin = vector2(0.0, 0.0)
+////                let sampleCount = vector2(Int32(widthCount), Int32(heightCount))
+////                return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
+////            }()
+////            for w in 0..<widthCount {
+////                for h in 0..<heightCount {
+////                    let vertex = SCNVector3(x: width*CGFloat(w)/CGFloat(widthCount-1)-width/2, y: 1-CGFloat(noiseMap.interpolatedValue(at: vector_float2(Float(w),Float(h)))), z: height*CGFloat(h)/CGFloat(heightCount-1)-height/2)
+////                    vertices.append(vertex)
+////                }
+////            }
+////
+////            var groundIndices = [UInt16]()
+////            var waterIndices = [UInt16]()
+////
+////            let index: (Int,Int) -> Int = {return $0 * heightCount + $1}
+////
+////
+////            for w in 0..<widthCount-1 {
+////                for h in 0..<heightCount-1 {
+////                    var squareVerticies = [UInt16]()
+////                    squareVerticies.append(UInt16(index(w,h)))
+////                    squareVerticies.append(UInt16(index(w,h+1)))
+////                    squareVerticies.append(UInt16(index(w+1,h)))
+////                    squareVerticies.append(UInt16(index(w+1,h+1)))
+////
+////                    var water: Bool = false
+////
+////                    for i in [index(w,h),index(w,h+1),index(w+1,h),index(w+1,h+1)] {
+////                        if vertices[i].y < 2 {
+////                            water = true
+////                        }
+////                    }
+////
+////                    groundIndices.append(squareVerticies[0])
+////                    groundIndices.append(squareVerticies[1])
+////                    groundIndices.append(squareVerticies[3])
+////
+////                    groundIndices.append(squareVerticies[0])
+////                    groundIndices.append(squareVerticies[3])
+////                    groundIndices.append(squareVerticies[2])
+////
+////                    if water == true {
+////                        waterIndices.append(squareVerticies[0])
+////                        waterIndices.append(squareVerticies[1])
+////                        waterIndices.append(squareVerticies[3])
+////
+////                        waterIndices.append(squareVerticies[0])
+////                        waterIndices.append(squareVerticies[3])
+////                        waterIndices.append(squareVerticies[2])
+////                    }
+////
+////                }
+////            }
+////        ground = Mesh(Verticies: vertices, Indices: groundIndices)
+////        ground.node.name = "Ground"
+////        let color1: NSColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+////        let color2: NSColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
+////        ground.node.geometry?.materials.first?.roughness.contents = 1
+////        ground.node.geometry?.materials.first!.shaderModifiers = [.geometry :
+////            getShader(from: "groundShader")
+////        ]
+////
+////
+////
+////
+////
+////        water = Mesh(Verticies: vertices, Indices: waterIndices)
+////        water.node.name = "Water"
+////
+////        let color: NSColor = #colorLiteral(red: 0, green: 0.9859127402, blue: 1, alpha: 0.5554098887)
+////        water.node.geometry?.materials.first!.diffuse.contents = color
+////        water.node.geometry?.materials.first?.metalness.contents = 1
+////        water.node.geometry?.materials.first?.roughness.contents = 0
+////        water.node.geometry?.materials.first?.multiply.contents = NSColor.white
+////        water.node.geometry?.materials.first?.shininess = 1
+////        water.node.geometry?.materials.first?.specular.contents = 1
+////        water.node.geometry?.materials.first!.shaderModifiers = [.geometry :
+////            getShader(from: "waterShader")
+////        ]
+////
+////
+////
+////
+////        pineGen = PineGenerator(NumberOfPines: 500, NoiseMap: noiseMap, Width: 400, Height: 400, widthCount: 100, heightCount: 100)
+////
+////    }
 //}
 
 class PineGenerator {
@@ -203,7 +202,7 @@ class PineGenerator {
             
         }
         for pine in pines {
-            pine.node.worldPosition = pine.position - SCNVector3(Width/2,-1.5,Height/2)
+            pine.node.worldPosition = pine.position - SCNVector3(Width/2,-2,Height/2)
         }
     }
     
@@ -216,7 +215,14 @@ class Pine {
     init(Position: SCNVector3) {
         self.position = Position
         let createSprite: SCNNode = {
-            let virtualObjectScene = SCNScene(named: "art.scnassets/pine.scn")!
+            let virtualObjectScene: SCNScene = {
+//                if Int.random(in: 0...1) == 0 {
+                    return SCNScene(named: "art.scnassets/polyTree.scn")!
+//                }else {
+//                    return SCNScene(named: "art.scnassets/pine.scn")!
+//                }
+                
+            }()
             
             let wrapperNode = SCNNode()
             
@@ -228,6 +234,7 @@ class Pine {
                 return wrapperNode
         }()
         self.node = createSprite
+        self.node.eulerAngles = SCNVector3(CGFloat.random(in: -0.2...0.2), CGFloat.random(in: 0...CGFloat.pi*2), CGFloat.random(in: -0.2...0.2))
     }
     
 }
